89764b85c5d9177464e2176b88eb60cdd3ce18e4
Enora <enora.bioinfo@gmail.com>
2026-04-24 09:53:23 +0200

    Add pages : Home, Login + Add services

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthPage } from './page/auth-page/auth-page';
+import { HomePage } from './page/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePage, title: "Home Page" },
+    { path: 'login', component: AuthPage, title: "Liste des Auteurs" },
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
