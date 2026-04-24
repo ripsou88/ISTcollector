69a3bb470d8eb453aaa2bb30b815a15285cd0aa0
User <cdomenger@orange.fr>
2026-04-24 13:04:45 +0200

    Ajout components cards + hero et MAJ dans les autres components

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -3,8 +3,9 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [ RouterModule ],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrlq: ['./header.css'],
 })
 export class Header {}
--- frontend/src/app/page/home-page/home-page.ts
+++ frontend/src/app/page/home-page/home-page.ts
@@ -1,30 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
   templateUrl: './home-page.html',
-  styleUrl: './home-page.css',
+  styleUrls: ['./home-page.css'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
+  //Menu hamburger
+  menuActive = false;
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+  }
+
+  //Bulle
+  bubbleVisible = false;
+
+  ngAfterViewInit() {
+    const trigger = document.querySelector('#middle-cards');
+    const bubble = document.querySelector('.bubble');
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && bubble) {
+          bubble.classList.add('show');
+          observer.disconnect();
+        }
+      });
+    }, { threshold: 0.3 });
+
+    if (trigger) {
+      observer.observe(trigger);
+    }
+  }
 }
-
-
-/* Il faut convertir le js en ts je pense
-document.addEventListener("DOMContentLoaded", function() {
-  const bubble = document.querySelector(".bubble");
-  const trigger = document.querySelector("#middle-cards");
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        bubble.classList.add("show");
-        observer.unobserve(trigger);
-      }
-    });
-  }, { threshold: 0.3 });
-
-  observer.observe(trigger);
-});
-*/
