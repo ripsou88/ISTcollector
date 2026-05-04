88e1fc18ffe9bcd0014c76f95fe3957cc6294aac
Hugo <hugo.pointier@gmail.com>
2026-05-04 16:27:59 +0200

    start of collection and implementation of types

---
+++ frontend/src/app/components/card-popup/card-popup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-popup',
+  imports: [],
+  templateUrl: './card-popup.html',
+  styleUrl: './card-popup.css',
+})
+export class CardPopup {
+
+}
---
+++ frontend/src/app/enum/type-prevention.ts
@@ -0,0 +1,5 @@
+export enum TypePrevention {
+  Medical = 'MEDICAL',
+  Barriere = 'BARRIERE',
+  Comportemental = 'COMPORTEMENTAL',
+}
