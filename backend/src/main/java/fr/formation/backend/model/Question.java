ad2e26da516307efdda67d91d16953991249036a
Adam <dede13210@gmail.com>
2026-05-04 16:55:43 +0200

    back: add question and reponse models

---
+++ backend/src/main/java/fr/formation/backend/model/Question.java
@@ -0,0 +1,78 @@
+package fr.formation.backend.model;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.Mapping;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+  
+    @Column(name = "question", nullable=false, length = 700)
+    private String questionString;
+
+    @OneToMany(mappedBy = "question")
+    private ArrayList<Reponse> reponses = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "id_bonne_reponse")
+    private Reponse bonneReponse;
+
+    public Question(String questionString, ArrayList<Reponse> reponses, Reponse bonneReponse) {
+        this.questionString = questionString;
+        this.reponses = reponses;
+        this.bonneReponse = bonneReponse;
+    }
+
+    public Question() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getQuestionString() {
+        return questionString;
+    }
+
+    public void setQuestionString(String questionString) {
+        this.questionString = questionString;
+    }
+
+    public ArrayList<Reponse> getReponses() {
+        return reponses;
+    }
+
+    public void setReponses(ArrayList<Reponse> reponses) {
+        this.reponses = reponses;
+    }
+
+    public Reponse getBonneReponse() {
+        return bonneReponse;
+    }
+
+    public void setBonneReponse(Reponse bonneReponse) {
+        this.bonneReponse = bonneReponse;
+    }
+
+    
+}
---
+++ backend/src/main/java/fr/formation/backend/model/Reponse.java
@@ -0,0 +1,64 @@
+package fr.formation.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "reponse")
+public class Reponse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "reponse", nullable = false , length = 700)
+    private String reponseString;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "id_question")
+    private Question question;
+
+    public Reponse(String reponseString, Question question) {
+        this.reponseString = reponseString;
+        this.question = question;
+    }
+
+    public Reponse(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getReponseString() {
+        return reponseString;
+    }
+
+    public void setReponseString(String reponseString) {
+        this.reponseString = reponseString;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+    
+    
+
+}
