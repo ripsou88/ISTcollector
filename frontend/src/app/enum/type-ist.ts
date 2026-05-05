e27984cfae1011505575da2ebef096575c96fbf2
Enora <enora.bioinfo@gmail.com>
2026-05-05 15:05:28 +0200

    Enum type IST

--- frontend/src/app/enum/type-ist.ts
+++ frontend/src/app/enum/type-ist.ts
@@ -5,3 +5,11 @@ export enum TypeIst {
   Bacterien = 'BACTERIEN',
   Viral = 'VIRAL',
 }
+
+export const TypeIstEmoji: Record<TypeIst, { emoji: string, label: string }> = {
+  [TypeIst.Bacterienne]: { emoji: '🦠', label: 'Bactérienne' },
+  [TypeIst.Virale]: { emoji: '🫟', label: 'Virale' },
+  [TypeIst.Parasitaire]: { emoji: '🪱', label: 'Parasitaire' },
+  [TypeIst.Bacterien]: { emoji: '🦠', label: 'Bactérien' },
+  [TypeIst.Viral]: { emoji: '🫟', label: 'Viral' },
+};
