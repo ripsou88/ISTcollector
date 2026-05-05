8e86665f2ac820aea0391751ce6f5090284050ac
gpichot <gael.pichot@2023.icam.fr>
2026-05-05 11:27:01 +0200

    Ajout de la table user-card pour la collection de l'utilisateur

--- backend/src/main/java/fr/formation/backend/model/User.java
+++ backend/src/main/java/fr/formation/backend/model/User.java
@@ -1,17 +1,32 @@
 package fr.formation.backend.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 @DiscriminatorValue("usr")
 public class User extends Compte {
-
+	
+	@ManyToMany
+	@JoinTable(
+	        name = "user-card",
+	        joinColumns = @JoinColumn(name = "user_id"),
+	        inverseJoinColumns = @JoinColumn(name = "ist_id")
+	    )
+	private List<Ist> ist = new ArrayList<>();
+	
     public User() {
     }
 
     public User(Integer id, String username, String password) {
         super(id, username, password);
     }
+    
 
 }
