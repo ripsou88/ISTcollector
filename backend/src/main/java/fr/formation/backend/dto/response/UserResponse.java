973002b8bb7bf6385a5ccb8b7dbda9904173ff83
Adam <dede13210@gmail.com>
2026-05-05 16:26:31 +0200

    BACK: fix user response

--- backend/src/main/java/fr/formation/backend/dto/response/UserResponse.java
+++ backend/src/main/java/fr/formation/backend/dto/response/UserResponse.java
@@ -5,10 +5,10 @@ import java.util.List;
 import fr.formation.backend.model.Ist;
 import fr.formation.backend.model.User;
 
-public record UserResponse(Integer id, String username, String password, List<Ist> ist ) {
+public record UserResponse(Integer id, String username, String password,Integer level, List<Ist> ist ) {
 
     public static UserResponse convert(User user) {
-        return new UserResponse(user.getId(), user.getUsername(), user.getPassword(), user.getIst());
+        return new UserResponse(user.getId(), user.getUsername(), user.getPassword(),user.getLevel(), user.getIst());
     }
 
 }
