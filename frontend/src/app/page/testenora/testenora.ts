57e76ab93993d11864d4773e5382f77ce2e966ae
Enora <enora.bioinfo@gmail.com>
2026-05-06 15:26:20 +0200

    Front : booster animations

--- frontend/src/app/page/testenora/testenora.ts
+++ frontend/src/app/page/testenora/testenora.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Booster } from '../../components/booster/booster';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-testenora',
-  imports: [Booster],
+  imports: [CommonModule, Booster],
   templateUrl: './testenora.html',
   styleUrl: './testenora.css',
 })
 export class Testenora {
-
+  protected showBooster: boolean = false;
 }
