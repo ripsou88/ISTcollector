1b2043862a2bafc0274966958dcfe98b48f1b609
Adam <dede13210@gmail.com>
2026-05-07 15:07:51 +0200

    FRONT: call increase level in booster opening

--- frontend/src/app/components/booster/booster.ts
+++ frontend/src/app/components/booster/booster.ts
@@ -1,4 +1,14 @@
+<<<<<<< Updated upstream
 import { Component } from '@angular/core';
+=======
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { CardsService } from '../../service/cards-service';
+import { Observable } from 'rxjs';
+import { Ist } from '../../interface/ist';
+import { CommonModule } from '@angular/common';
+import { CardPlaceholder } from '../card-placeholder/card-placeholder';
+import { QuizzService } from '../../service/quizz-service';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-booster',
@@ -8,4 +18,34 @@ import { Component } from '@angular/core';
 })
 export class Booster {
   protected isOpen: boolean = false;
+<<<<<<< Updated upstream
+=======
+  protected showCards: boolean = false;
+  protected boosterCards: Ist[] = [];
+  private cards: CardsService = inject(CardsService);
+  private quizzService: QuizzService = inject(QuizzService);
+
+  private cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+
+boosterOuverture() {
+  console.log("boosterOuverture appelée");
+    this.isOpen = true;
+
+    this.cards.getThreeRandom().subscribe(cards => {
+      this.boosterCards = cards;
+      this.cdr.detectChanges();
+    });
+
+    this.quizzService.increaseLevel().subscribe();
+
+    setTimeout(() => {
+      this.showCards = true;
+      this.cdr.detectChanges();
+    }, 500);
+}
+
+quitter() {
+
+}
+>>>>>>> Stashed changes
 }
